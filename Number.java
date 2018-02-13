536c13e6a5343fd6a417bdbd228d081c4538905d
Gringger <32059567+Gringger@users.noreply.github.com>
2018-02-13 06:01:40 +0200

    First

---
+++ Number.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Number {
+	 long i;
+     long j;
+     long mxcnt=1;
+     public void run(){
+    	 scanner();
+    	 bigger();
+    	 count();
+    	 
+     }
+
+private void scanner(){
+	Scanner fu1 = new Scanner(System.in);
+    while (fu1.hasNextInt()) { 
+    	 i = fu1.nextInt(); 
+    	 j = fu1.nextInt();
+        fu1.close();
+}}
+private void bigger(){
+	      if (i>j){ 
+		 long time;
+		 time=i; i=j; j=time;
+		}
+}
+    private void count(){
+    	long k;
+    long n=i;
+	 while(n<=j){
+		 k=n;
+		 int cnt;
+		 cnt=1;
+		 
+		 while(k!=1){					 
+		 if (k % 2 != 0)
+			{
+				k = 3 * k + 1;
+				cnt++;
+			}
+		 else{	
+				k = k / 2;
+				cnt++;
+		 }
+		 
+		 }
+		 if (cnt>mxcnt){
+			 mxcnt=cnt;
+		 }
+		 n++;
+	}
+    	System.out.println(i+" "+j+" " + mxcnt);
+	
+    }
+    
+
+	public static void main(String[] args) {
+		new Number();
+	
+
+	}
+}
+
